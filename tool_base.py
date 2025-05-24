61277c1eb4183ef59a610033035a33a557a0c280
Adel Haj Jumah <adelhajjumah@gmail.com>
2025-05-24 11:30:43 +0200

    Delete tool_base.py

--- tool_base.py
+++
@@ -1,6 +0,0 @@
-class ToolBase:
-    name = "UnnamedTool"
-    description = ""
-
-    async def invoke(self, input_text: str) -> str:
-        raise NotImplementedError("Tool muss 'invoke()' implementieren.")

16cce839a6ad24973b49f4d9f00ad761ebb208e1
Adel Haj Jumah <adelhajjumah@gmail.com>
2025-05-24 11:29:04 +0200

    Delete tools directory

--- tools/web_tool.py
+++
@@ -1,9 +0,0 @@
-from web_search_agent import suche_im_web
-
-class WebSearchTool:
-    name = "WebSearchTool"
-    description = "Führt eine Google Websuche aus (SerpAPI)."
-
-    def __call__(self, query: str):
-        ergebnisse = suche_im_web(query)
-        return "\n".join([f"{e['Titel']}: {e['Beschreibung']}" for e in ergebnisse[:3]]) or "Keine Web-Ergebnisse."
